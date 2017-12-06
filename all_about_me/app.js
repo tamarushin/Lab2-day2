97b80e345f8d8e633494a2c317968570070a9ad7
tamarushin <tamarushin@gmail.com>
2017-12-06 10:15:44 -0800

    added more if-else statements

--- all_about_me/app.js
+++ all_about_me/app.js
@@ -5,20 +5,44 @@ alert('Hi there ' + user + ', I\'m going to ask you some questions so that you c
 //First Question
 var moonBeams = prompt('how many moon beams has tama caught when on pluto?');
 console.log('number of moonbeams:', moonBeams);
-if (moonBeams === 14 || moonBeams === 10){
+if (moonBeams === 1 || moonBeams === 10){
   alert ('correct Stargazer!');
+else ('Oh bummer, that is incorrect!');
 }
+
 //Second Question
 var saturnBurns = prompt('how many saturn burns did tama get when going to highschool?');
 console.log('number of saturn burns:', saturnBurns);
 if (saturnBurns === 2 || saturnBurns === 3) {
   alert ('OUCH! but thats correct!');
+else ('Nope! Wrong answer!');
 }
+
 //Third Question
 var venus = prompt('is tama part venician?\n(Please answer "y" for Yes and "n" for No)');
 console.log('tama part venician:', venus);
+if (venus === 'y');{
+  alert ('Good Guess! Tama is part venician');
+}
+if (venus === 'n');{
+  alert ('Oh Earthling, No good! Tama is part venician!');
+}
+
 //Forth Question
 var mars = prompt('Did Tama work on Mars?\n(Please answer "y" for Yes and "n" for No');
 console.log('work on mars:', mars);
+if (mars === 'y');{
+  alert ('Yep! Tama worked at the Mars Club!');
+}
+if (mars === 'n');{
+  alert ('Nope! I did! I served Mars Bars to Martians!');
+}
 //Fifth Question
 var blackHole = prompt('Did Tama go to highschool in a black hole in the middle of the universe?\n(Please answer "y" for Yes and "n" for No)');
+console.log('blackHole:', blackHole)
+if (blackHole === 'y');{
+  alert ('Correctamundo! It was called Hello!Hello!Hello!Hello!, like an echo');
+}
+if (blackHole === 'n');{
+  alert ('That is the wrong answer')
+}
