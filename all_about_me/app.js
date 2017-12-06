d0cc3f48037fe5f099268fa7ef2eb32013e70f9c
tamarushin <tamarushin@gmail.com>
2017-12-05 23:02:17 -0800

    worked on all files

---
+++ all_about_me/app.js
@@ -0,0 +1,24 @@
+'use strict';
+var user = prompt('Welcome earthling! What is your name?');
+console.log('User:', user);
+alert('Hi there ' + user + ', I\'m going to ask you some questions so that you can get to know me. After you answer the questions, please read my Bio!');
+//First Question
+var moonBeams = prompt('how many moon beams has tama caught when on pluto?');
+console.log('number of moonbeams:', moonBeams);
+if (moonBeams === '14' || moonBeams === '10'){
+  alert ('correct Stargazer!');
+}
+//Second Question
+var saturnBurns = prompt('how many saturn burns did tama get when going to highschool?');
+console.log('number of saturn burns:', saturnBurns);
+if (saturnBurns === '2' || saturnBurns === '3') {
+  alert ('OUCH! but thats correct!');
+}
+//Third Question
+var venus = prompt('is tama part venician?\n(Please answer "y" for Yes and "n" for No)');
+console.log('tama part venician:', venus);
+//Forth Question
+var mars = prompt('Did Tama work on Mars?\n(Please answer "y" for Yes and "n" for No');
+console.log('work on mars:', mars);
+//Fifth Question
+var blackHole = prompt('Did Tama go to highschool in a black hole in the middle of the universe?\n(Please answer "y" for Yes and "n" for No)');
