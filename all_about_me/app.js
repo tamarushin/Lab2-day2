88e71d70c9c384ff4eed4d2ea21752e09814d9af
tamarushin <tamarushin@gmail.com>
2017-12-06 08:59:34 -0800

    took off quotes around numbers

--- all_about_me/app.js
+++ all_about_me/app.js
@@ -5,13 +5,13 @@ alert('Hi there ' + user + ', I\'m going to ask you some questions so that you c
 //First Question
 var moonBeams = prompt('how many moon beams has tama caught when on pluto?');
 console.log('number of moonbeams:', moonBeams);
-if (moonBeams === '14' || moonBeams === '10'){
+if (moonBeams === 14 || moonBeams === 10){
   alert ('correct Stargazer!');
 }
 //Second Question
 var saturnBurns = prompt('how many saturn burns did tama get when going to highschool?');
 console.log('number of saturn burns:', saturnBurns);
-if (saturnBurns === '2' || saturnBurns === '3') {
+if (saturnBurns === 2 || saturnBurns === 3) {
   alert ('OUCH! but thats correct!');
 }
 //Third Question
